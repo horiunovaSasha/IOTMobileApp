fe1399cc54ce1bb5ff6d7c6e90de6d671501cf41
Koshman Oleksandra <horiunova.sahsa@gmail.com>
2020-03-04 17:17:58 +0200

    added brightness page

---
+++ IOTMobileApp/IOTMobileApp/Views/BrightnessPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace IOTMobileApp.Views
+{
+    [DesignTimeVisible(false)]
+    public partial class BrightnessPage
+    {
+        public BrightnessPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
--- IOTMobileApp/IOTMobileApp/Views/PlakPage.xaml.cs
+++ IOTMobileApp/IOTMobileApp/Views/PlakPage.xaml.cs
@@ -8,12 +8,11 @@ namespace IOTMobileApp.Views
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : TabbedPage
+    public partial class PlakPage : ContentPage
     {
-        public MainPage(): base()
+        public PlakPage()
         {
             InitializeComponent();
-            NavigationPage.SetHasNavigationBar(this, false);
         }
     }
 }
